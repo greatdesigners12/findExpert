0d74e7b3ee0401817fc712ece10efeef3354d24b
WSNekoWay <banthony03@student.ciputra.ac.id>
2023-11-04 19:06:36 +0700

    up lex

--- src/controller/experts_controller/experts_controller.js
+++ src/controller/experts_controller/experts_controller.js
@@ -1,2 +1,92 @@
+import { db } from "../firebaseApp";
+import { collection, doc, getDocs, setDoc, updateDoc, deleteDoc } from "firebase/firestore";
+import { Expert } from "./models/expert"; 
+import { ResultData } from "../structureJson/resultData";
 
 
+export class ExpertsController {
+    async getExpertById(id) {
+        const result = new ResultData();
+
+        try {
+            const expertsCollection = collection(db, "expertData"); // Replace with your collection name
+            const expertSnapshot = await getDocs(expertsCollection);
+
+            for (const expertDoc of expertSnapshot.docs) {
+                const expertData = expertDoc.data();
+                if (expertData.id === id) {
+                    result.data = new Expert(
+                        expertData.fullName,
+                        expertData.phoneNumber,
+                        expertData.email,
+                        expertData.password,
+                        expertData.birthDate,
+                        expertData.gender,
+                        expertData.education,
+                        expertData.fieldId,
+                        expertData.nik,
+                        expertData.jobExperience,
+                        expertData.ktp,
+                        expertData.certificates,
+                        expertData.profilePicture
+                    );
+                    result.errorMessage = "";
+                    result.statusCode = 200;
+                    return result;
+                }
+            }
+
+            result.data = null;
+            result.errorMessage = "Expert not found";
+            result.statusCode = 404;
+        } catch (error) {
+            result.data = null;
+            result.errorMessage = "Failed to get expert: " + error.message;
+            result.statusCode = 500;
+        }
+
+        return result;
+    }
+
+    async updateExpert(id, newData) {
+        const result = new ResultData();
+
+        try {
+            const expertsCollection = collection(db, "expertData"); 
+            const expertRef = doc(expertsCollection, id);
+
+            await updateDoc(expertRef, newData);
+
+            result.data = newData;
+            result.errorMessage = "";
+            result.statusCode = 200;
+        } catch (error) {
+            result.data = null;
+            result.errorMessage = "Failed to update expert: " + error.message;
+            result.statusCode = 500;
+        }
+
+        return result;
+    }
+
+    async deleteExpert(id) {
+        const result = new ResultData();
+
+        try {
+            const expertsCollection = collection(db, "expertData"); 
+            const expertRef = doc(expertsCollection, id);
+
+            await deleteDoc(expertRef);
+
+            result.data = null;
+            result.errorMessage = "";
+            result.statusCode = 200;
+        } catch (error) {
+            result.data = null;
+            result.errorMessage = "Failed to delete expert: " + error.message;
+            result.statusCode = 500;
+        }
+
+        return result;
+    }
+}
--- src/controller/experts_controller/models/expert.js
+++ src/controller/experts_controller/models/expert.js
@@ -1,29 +1,61 @@
-export class Expert{
-    constructor(fullName, phoneNumber, email, password, birthDate, gender, education, fieldId, nik, jobExperience, ktp, certificates, profilePicture){
-        this.fullName = fullName
-        this.phoneNumber = phoneNumber
-        this.email = email 
-        this.password = password
-        this.birthDate = birthDate
-        this.gender = gender
-        this.education = education
-        this.fieldId = fieldId
-        this.nik = nik
-        this.jobExperience = jobExperience
-        this.ktp = ktp 
-        this.certificates = certificates
-        this.profilePicture = profilePicture
-        this.status = "unverified"
+export class Expert {
+    constructor(fullName, phoneNumber, email, password, birthDate, gender, education, fieldId, nik, jobExperience, ktp, certificates, profilePicture, open, close) {
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.password = password;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.education = education;
+        this.fieldId = fieldId;
+        this.nik = nik;
+        this.jobExperience = jobExperience;
+        this.ktp = ktp;
+        this.certificates = certificates;
+        this.profilePicture = profilePicture;
+        this.status = "unverified";
+        this.open = open; // New variable
+        this.close = close; // New variable
     }
 
-    serialize(){
-        return {"fullName" : this.fullName, "phoneNumber" : this.phoneNumber, "email" : this.email, "password" : this.password, "birthDate" : this.birthDate, "gender" : this.gender, 
-        "education" : this.education, "fieldId" : this.fieldId, "nik" : this.nik, "jobExperience" : this.jobExperience, "ktp" : this.ktp, "certificates" : this.certificates, "profilePicture"
-        : this.profilePicture, "status" : this.status}
+    serialize() {
+        return {
+            "fullName": this.fullName,
+            "phoneNumber": this.phoneNumber,
+            "email": this.email,
+            "password": this.password,
+            "birthDate": this.birthDate,
+            "gender": this.gender,
+            "education": this.education,
+            "fieldId": this.fieldId,
+            "nik": this.nik,
+            "jobExperience": this.jobExperience,
+            "ktp": this.ktp,
+            "certificates": this.certificates,
+            "profilePicture": this.profilePicture,
+            "status": this.status,
+            "open": this.open, // New variable
+            "close": this.close, // New variable
+        };
     }
 
-    checkIfThereIsEmpty(){
-        return this.fullName === "" || this.phoneNumber === "" || this.email === "" || this.password === "" || this.birthDate === "" || this.gender === "" || this.education === "" || this.fieldId === ""
-        || this.nik === "" || this.jobExperience === "" || this.ktp === "" || this.certificates === null || this.profilePicture === ""
+    checkIfThereIsEmpty() {
+        return (
+            this.fullName === "" ||
+            this.phoneNumber === "" ||
+            this.email === "" ||
+            this.password === "" ||
+            this.birthDate === "" ||
+            this.gender === "" ||
+            this.education === "" ||
+            this.fieldId === "" ||
+            this.nik === "" ||
+            this.jobExperience === "" ||
+            this.ktp === "" ||
+            this.certificates === null ||
+            this.profilePicture === "" ||
+            this.open === "" || // New variable
+            this.close === "" // New variable
+        );
     }
 }
