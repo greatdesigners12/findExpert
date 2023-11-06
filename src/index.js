5ee9af9d936bb34990f253f3c9aad362bc3d7996
CallistaEve <eyaurentius@gmail.com>
2023-11-06 10:52:00 +0700

    router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { createBrowserRouter } from 'react-router-dom';
+import { HomePage } from './homepage';
+import ExpertDetail from './expertDetail';
+const router = createBrowserRouter([{
+    path : "/", element : <HomePage/>,
+    path : "/expertDetail", element : <ExpertDetail/>,
+}])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
